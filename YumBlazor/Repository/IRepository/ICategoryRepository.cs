6c8331cb9e2e62d5d513286bd5562d4b09ce1b09
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-08 11:54:29 -0500

    Section 12 - Create ICategoryRepository

---
+++ YumBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+	public interface ICategoryRepository
+	{
+		public Category Create(Category obj);
+		public Category Update(Category obj);
+		public bool Delete(int id);
+		public Category Get(int id);
+		public IEnumerable<Category> GetAll();
+	}
+}
