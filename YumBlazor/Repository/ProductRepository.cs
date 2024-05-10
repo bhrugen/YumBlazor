f5b288fd52421bf691c42f307c3acb2f19bfbbdc
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-10 14:47:03 -0500

    Section 13 - Upload Product

--- YumBlazor/Repository/ProductRepository.cs
+++ YumBlazor/Repository/ProductRepository.cs
@@ -42,7 +42,7 @@ namespace YumBlazor.Repository
 
 		public async Task<IEnumerable<Product>> GetAllAsync()
 		{
-			return await _db.Product.ToListAsync();
+			return await _db.Product.Include(u=>u.Category).ToListAsync();
 		}
 
 		public async Task<Product> UpdateAsync(Product obj)
