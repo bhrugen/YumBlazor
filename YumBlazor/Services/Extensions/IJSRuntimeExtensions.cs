db06b349cdcb9f02787e40f7662376b458935fbe
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-10 11:20:00 -0500

    Section 12 - Toastr Notifications

---
+++ YumBlazor/Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ YumBlazor/wwwroot/js/notification.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
