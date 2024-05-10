bc9c9fe9623d321d8142fc9ad5b22b07a8d893bb
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-10 13:19:42 -0500

    Section 13 - Create Product Model

---
+++ YumBlazor/Data/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YumBlazor.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(0.01, 1000)]
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? SpecialTag { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+    }
+}
