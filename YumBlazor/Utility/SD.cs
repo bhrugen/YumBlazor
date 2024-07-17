a0d2a10f6b3d8273992b40b0da4946afc3e7d403
Bhrugen Patel <bhrugen1990@gmail.com>
2024-07-17 15:15:55 -0500

    Section 14 - Role Dropdown

---
+++ YumBlazor/Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace YumBlazor.Utility
+{
+    public static class SD
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+    }
+}
