3f1237a2c68d69640adaa5bc876b2e957ac2e0c9
Bhrugen Patel <bhrugen1990@gmail.com>
2024-07-19 15:17:33 -0500

    Section 16 - Shopping Cart Interface

---
+++ YumBlazor/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface IShoppingCartRepository
+    {
+        public Task<bool> UpdateCartAsync(string userId, int product, int updateBy);
+        public Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
+        public Task<bool> ClearCartAsync(string? userId);
+    }
+}
