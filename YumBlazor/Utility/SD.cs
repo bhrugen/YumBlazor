17def521fbeb4ea4820d6d73c688acc38fa07f19
Bhrugen Patel <bhrugen1990@gmail.com>
2024-07-22 16:48:59 -0500

    Section 17 - Convert shopping cart to order detail

--- YumBlazor/Utility/SD.cs
+++ YumBlazor/Utility/SD.cs
@@ -1,4 +1,6 @@
-﻿namespace YumBlazor.Utility
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Utility
 {
     public static class SD
     {
@@ -9,5 +11,24 @@
         public static string StatusReadyForPickUp = "ReadyForPickUp";
         public static string StatusCompleted = "Completed";
         public static string StatusCancelled = "Cancelled";
+
+        public static List<OrderDetail> ConvertShoppingCartListToOrderDetail(List<ShoppingCart> shoppingCarts)
+        {
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+
+            foreach (var cart in shoppingCarts)
+            {
+                OrderDetail orderDetail = new OrderDetail
+                {
+                    ProductId = cart.ProductId,
+                    Count = cart.Count,
+                    Price = Convert.ToDouble(cart.Product.Price),
+                    ProductName = cart.Product.Name
+                };
+                orderDetails.Add(orderDetail);
+            }
+
+            return orderDetails;
+        }
     }
 }
