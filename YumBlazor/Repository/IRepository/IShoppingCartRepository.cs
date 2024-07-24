93ab4db4b714150a5a19eedf16ece64f82b7c838
Bhrugen Patel <bhrugen1990@gmail.com>
2024-07-24 15:02:49 -0500

    Section 20 - Get Items in Cart for a User

--- YumBlazor/Repository/IRepository/IShoppingCartRepository.cs
+++ YumBlazor/Repository/IRepository/IShoppingCartRepository.cs
@@ -7,5 +7,7 @@ namespace YumBlazor.Repository.IRepository
         public Task<bool> UpdateCartAsync(string userId, int product, int updateBy);
         public Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
         public Task<bool> ClearCartAsync(string? userId);
-    }
+
+        public Task<int> GetTotalCartCartCountAsync(string? userId);
+	}
 }
--- YumBlazor/Repository/ShoppingCartRepository.cs
+++ YumBlazor/Repository/ShoppingCartRepository.cs
@@ -24,7 +24,19 @@ namespace YumBlazor.Repository
             return await _db.ShoppingCart.Where(u => u.UserId == userId).Include(u => u.Product).ToListAsync();
         }
 
-        public async Task<bool> UpdateCartAsync(string userId, int productId, int updateBy)
+		public async Task<int> GetTotalCartCartCountAsync(string? userId)
+		{
+            int cartCount = 0;
+            var cartItems = await _db.ShoppingCart.Where(u => u.UserId == userId).ToListAsync();
+
+            foreach(var item in cartItems)
+            {
+                cartCount+= item.Count;
+            }
+			return cartCount;
+		}
+
+		public async Task<bool> UpdateCartAsync(string userId, int productId, int updateBy)
         {
             if (string.IsNullOrEmpty(userId))
             {
