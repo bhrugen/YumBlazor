db13ea5d09e156731d907ccbd60e94f8eb2f793a
Bhrugen Patel <bhrugen1990@gmail.com>
2024-07-26 12:44:57 -0500

    Section 22 - Payment service to create stripe session

---
+++ YumBlazor/Services/PaymentService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Stripe.Checkout;
+using Stripe.Climate;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Services
+{
+    public class PaymentService
+    {
+        private readonly NavigationManager _navigationManager;
+        private readonly IOrderRepository _orderRepository;
+
+        public PaymentService(NavigationManager navigationManager, IOrderRepository orderRepository)
+        {
+            _navigationManager = navigationManager;
+            _orderRepository = orderRepository;
+        }
+
+        public Session CreateStripeCheckoutSession(OrderHeader orderHeader)
+        {
+
+            var lineItems = orderHeader.OrderDetails
+                .Select(order => new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        Currency = "usd",
+                        UnitAmountDecimal = (decimal?)order.Price * 100,  //10.10-> 1010,
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = order.ProductName
+                        }
+
+                    },
+                    Quantity = order.Count
+                }).ToList();
+
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                SuccessUrl = $"{_navigationManager.BaseUri}order/confirmation/{{CHECKOUT_SESSION_ID}}",
+                CancelUrl= $"{_navigationManager.BaseUri}cart",
+                LineItems = lineItems,
+                Mode = "payment",
+            };
+
+            var service = new SessionService();
+            var session = service.Create(options);
+
+            return session;
+        }
+    }
+}
